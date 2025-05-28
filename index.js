fe4a170121de0d0cc15405e8f14033ec1ee449b2
wendywen20 <wenyimei6699@gmail.com>
2025-05-28 13:41:30 -0400

    CRUD

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ app.get("/todos/:id", (req,res)=>{
         res.json(todosAPI.find((todo)=>todo.id===id))
         })
 app.post("/todos", (req,res)=>{
-    const {title, completed} = req.body
-    if(!title || !completed){return res.status(400).json({error:"Title and completed status are required"})}
-    const newTodo={ id: todosAPI.length + 1, title, completed }
-    todosAPI=todosAPI.push(newTodo)
+    const {title} = req.body
+    if(!title || typeof title !=="string"){return res.status(400).json({error:"title is required and must be a string"})}
+    const newTodo={ id: todosAPI.length + 1, title, completed:false }
+    todosAPI.push(newTodo)
     res.status(201).json(newTodo);
     })
 //delete
@@ -52,12 +52,23 @@ app.delete("/todos/:id", (req,res)=>{
     res.status(204).send()
 })
 
-//put update a todo
+//put update a todo by id
 app.put("/todos/:id", (req,res)=>{
     const id=parseInt(req.params.id);
-    const {task, completed}=req.body
-    todosAPI=todosAPI.filter(todo=>todo.id!== id);
-    res.status(201).json(todosAPI);
+    const {title, completed}=req.body
+    const todoEdit=todosAPI.find(todo=>todo.id=== id);
+    if(!todoEdit){
+        return res.status(404).json({error: "Todo not found"})
+    }
+    if(!title || typeof title!=="string"){
+        return res.status(404).json({error: "title is needed and title must be string"})
+    }
+    if(!completed || typeof completed !=="boolean"){
+        return res.status(404).json({error: "completed status is needed and completed status  must be true or false"})
+    }
+    todoEdit.title=title
+    todoEdit.completed=completed
+    res.json(todoEdit);
 })
 
 app.listen(port,()=>{
